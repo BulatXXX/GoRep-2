eadcfe0602ea1a4c1d1591e546bedcbfebbbb71b
Sattarov Bulat <90152783+BulatXXX@users.noreply.github.com>
2025-09-09 20:41:45 +0300

    init

---
+++ cmd/myapp/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/BulatXXX/GoRep-2/internal/app"
+)
+
+func main() {
+	app.Run()
+}
---
+++ internal/app/app.go
@@ -0,0 +1,44 @@
+package app
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/BulatXXX/GoRep-2/internal/app/handlers"
+	"github.com/BulatXXX/GoRep-2/utils"
+)
+
+func Run() {
+	mux := http.NewServeMux()
+
+	// Корневой маршрут
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		utils.LogRequest(r)
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		fmt.Fprintln(w, "Hello, Go project structure!")
+	})
+
+	mux.HandleFunc("/ping", handlers.Ping)
+
+	mux.HandleFunc("/fail", func(w http.ResponseWriter, r *http.Request) {
+		utils.LogRequest(r)
+		utils.WriteErr(w, http.StatusBadRequest, "bad_request_example")
+	})
+
+	handler := withRequestID(mux)
+	utils.LogInfo("Server is starting on :8080")
+	if err := http.ListenAndServe(":8080", handler); err != nil {
+		utils.LogError("server error: " + err.Error())
+	}
+}
+
+func withRequestID(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := r.Header.Get("X-Request-Id")
+		if id == "" {
+			id = utils.NewID16()
+		}
+		w.Header().Set("X-Request-Id", id)
+		next.ServeHTTP(w, r)
+	})
+}
